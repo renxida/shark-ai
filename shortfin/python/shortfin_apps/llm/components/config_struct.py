c75be75d19a0937f54725decdacb49a96dee6da3
Xida <xidaren2@amd.com>
2025-01-29 21:56:12 +0000

    docstring improvements

--- shortfin/python/shortfin_apps/llm/components/config_struct.py
+++ shortfin/python/shortfin_apps/llm/components/config_struct.py
@@ -4,56 +4,12 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-"""Configuration objects.
+"""
+Configuration objects.
 
-Parameters that are intrinsic to a specific model.
-
-In a typical transformer model, the KV cache is organized similar to (mapped to
-our parameter names below):
-    k = tensor.empty(transformer_block_count, batch_size, seq,
-                    attn_head_count_kv, attn_head_dim)
-    v = ...
-
-For context, a popular model has parameters of:
-    attn_dtype_size = 2  # (fp16)
-    max_seq_len = 2048
-    transformer_block_count = 32
-    attn_head_count_kv = 32
-    attn_head_dim = 128   # (dim / head_count)
-
-If paging, then we primarily care about the organization of a single block, where
-a block represents a single position in the sequence for a single item in the batch.
-Therefore, it will be organized like:
-    block = torch.empty(transformer_block_count, 2, attn_head_count_kv, attn_head_dim)
-
-In this scenario, we declare that one block holds the KV cache for all transformer
-block layers because it reduces the accounting. As such, for the above example,
-a single position in the sequence will be 524,288 bytes, assuming a 2-byte element
-type. If we choose to block by block_stride=16 positions, each block will be 8MiB.
-Assuming we wanted to dedicate 12GiB to the block cache, this would equate to 1536
-blocks for a total number of sequence positions of 24,576.
-
-These are well-known numbers but are derived above to give a sense of scale.
-
-In order to indirect through to the block cache, we have to provide the index map
-to specific invocations:
-
-* Prefill: Prefill is only writing to the blocks from [0:prompt_len], so it will
-    need write indices of [batch_size, prompt_len // block_stride + 1].
-* Decode step: Decode is auto-regressive, and needs to first compute the new kv
-    row and then attend over all rows in the cache up to this point in the sequence.
-
-If wanting to avoid dynamic allocation of transients, we can also pool the index
-tables based on the maximum batch size and maximum sequence length. Since all
-block cache sizes are well within the range of an i16, we will use that for storage.
-Therefore, each batch invocation would need a block lookup table of:
-
-    byte_size = max_batch_size * (max_seq_len // block_stride) * sizeof(int16_t)
-
-For a max_batch_size of 16, this is 4KiB of block index table lookups per
-invocation. We don't have to statically allocate this, but the system is more
-predictable if we just reserve what we need. Again, numbers are given to give a
-sense of scale only: real workloads will vary.
+Classes:
+- ModelParams: for reading and managing config keys specified in `config.json` files exported by `python -m sharktank.examples.export_paged_llm_v1`
+- ServerParams: for specifying config keys needed by `python -m shortfin_apps.llm.server`
 """
 
 import json
@@ -81,7 +37,55 @@ dataclasses_json.cfg.global_config.decoders[sfnp.DType] = _decode_dtype
 @dataclass_json(undefined=Undefined.RAISE)
 @dataclass
 class PagedKVCacheParams:
-    """Parameters for the paged KV cache."""
+    """Parameters for the paged KV cache.
+
+    In a typical transformer model, the KV cache is organized similar to (mapped to
+    our parameter names below):
+        k = tensor.empty(transformer_block_count, batch_size, seq,
+                        attn_head_count_kv, attn_head_dim)
+        v = ...
+
+    For context, a popular model has parameters of:
+        attn_dtype_size = 2  # (fp16)
+        max_seq_len = 2048
+        transformer_block_count = 32
+        attn_head_count_kv = 32
+        attn_head_dim = 128   # (dim / head_count)
+
+    If paging, then we primarily care about the organization of a single block, where
+    a block represents a single position in the sequence for a single item in the batch.
+    Therefore, it will be organized like:
+        block = torch.empty(transformer_block_count, 2, attn_head_count_kv, attn_head_dim)
+
+    In this scenario, we declare that one block holds the KV cache for all transformer
+    block layers because it reduces the accounting. As such, for the above example,
+    a single position in the sequence will be 524,288 bytes, assuming a 2-byte element
+    type. If we choose to block by block_stride=16 positions, each block will be 8MiB.
+    Assuming we wanted to dedicate 12GiB to the block cache, this would equate to 1536
+    blocks for a total number of sequence positions of 24,576.
+
+    These are well-known numbers but are derived above to give a sense of scale.
+
+    In order to indirect through to the block cache, we have to provide the index map
+    to specific invocations:
+
+    * Prefill: Prefill is only writing to the blocks from [0:prompt_len], so it will
+        need write indices of [batch_size, prompt_len // block_stride + 1].
+    * Decode step: Decode is auto-regressive, and needs to first compute the new kv
+        row and then attend over all rows in the cache up to this point in the sequence.
+
+    If wanting to avoid dynamic allocation of transients, we can also pool the index
+    tables based on the maximum batch size and maximum sequence length. Since all
+    block cache sizes are well within the range of an i16, we will use that for storage.
+    Therefore, each batch invocation would need a block lookup table of:
+
+        byte_size = max_batch_size * (max_seq_len // block_stride) * sizeof(int16_t)
+
+    For a max_batch_size of 16, this is 4KiB of block index table lookups per
+    invocation. We don't have to statically allocate this, but the system is more
+    predictable if we just reserve what we need. Again, numbers are given to give a
+    sense of scale only: real workloads will vary.
+    """
 
     # Tokens per page.
     block_seq_stride: int
@@ -200,7 +204,13 @@ def human_size(num, suffix="B"):
 @dataclass_json(undefined=Undefined.RAISE)
 @dataclass
 class ServerParams:
-    """Server configuration parameters."""
+    """
+    Parameters relevant to a specific server launch.
+
+    shortfin_apps.llm.server accepts an optional json server_config file for this + commandline arguments for setting attributes of this class.
+
+    Commandline args take priority over server_config.json, which takes precedence over defaults defined in this dataclass.
+    """
 
     # KV cache configuration
     prefix_sharing_algorithm: str = "none"  # none or trie
