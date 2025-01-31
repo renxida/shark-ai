d3b8e08c6440f4bc3b6ea7dcdae9a985d73aff7c
Cedar <cedar.ren@gmail.com>
2025-01-31 11:19:32 -0800

    move to mi250

--- app_tests/integration_tests/llm/device_settings.py
+++ app_tests/integration_tests/llm/device_settings.py
@@ -24,15 +24,29 @@ GFX942 = DeviceSettings(
     server_flags=("--device=hip",),
 )
 
+GFX90A = DeviceSettings(
+    compile_flags=(
+        "--iree-hal-target-backends=rocm",
+        "--iree-hip-target=gfx90a",
+    ),
+    server_flags=("--device=hip",),
+)
+
+
 def get_device_based_on_env_variable():
     import os
 
-    device_name = os.environ.get("SHORTFIN_INTEGRATION_TEST_DEVICE", "cpu").lower()
+    device_name = os.environ.get(
+        "SHORTFIN_INTEGRATION_TEST_DEVICE", "unspecified"
+    ).lower()
+    if device_name == "unspecified":
+        raise ValueError(
+            "os.environ['SHORTFIN_INTEGRATION_TEST_DEVICE'] is not set. Set it to one of the supported device names: 'gfx942', 'gfx90a', 'host', 'hostcpu', 'local-task', 'cpu'"
+        )
 
     table = {
-        "gpu": GFX942,
-        "amdgpu": GFX942,
         "gfx942": GFX942,
+        "gfx90a": GFX90A,
         "host": CPU,
         "hostcpu": CPU,
         "local-task": CPU,
