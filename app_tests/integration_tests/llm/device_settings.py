947c77199797b0c9f65201940ff6563061406fdc
Xida <xidaren2@amd.com>
2025-02-03 18:15:52 +0000

    make device_name a pytest option rather than an environment variable

--- app_tests/integration_tests/llm/device_settings.py
+++ app_tests/integration_tests/llm/device_settings.py
@@ -32,27 +32,24 @@ GFX90A = DeviceSettings(
     server_flags=("--device=hip",),
 )
 
+table = {
+    "gfx942": GFX942,
+    "gfx90a": GFX90A,
+    "host": CPU,
+    "hostcpu": CPU,
+    "local-task": CPU,
+    "cpu": CPU,
+}
 
-def get_device_based_on_env_variable():
-    import os
 
-    device_name = os.environ.get(
-        "SHORTFIN_INTEGRATION_TEST_DEVICE", "unspecified"
-    ).lower()
-    if device_name == "unspecified":
-        raise ValueError(
-            "os.environ['SHORTFIN_INTEGRATION_TEST_DEVICE'] is not set. Set it to one of the supported device names: 'gfx942', 'gfx90a', 'host', 'hostcpu', 'local-task', 'cpu'"
-        )
+def get_device_settings_by_name(device_name):
+    """
+    Get device settings by name.
 
-    table = {
-        "gfx942": GFX942,
-        "gfx90a": GFX90A,
-        "host": CPU,
-        "hostcpu": CPU,
-        "local-task": CPU,
-        "cpu": CPU,
-    }
-    if device_name in table:
+    See table below for options:
+    """
+
+    if device_name.lower() in table:
         return table[device_name]
 
     raise ValueError(
