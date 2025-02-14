2157ba299065dab838decd7d9ccacea3086469c4
Cedar <cedar.ren@gmail.com>
2025-02-14 15:13:20 -0800

    oneliner solution to mocking

--- shortfin/tests/apps/llm/components/messages_test.py
+++ shortfin/tests/apps/llm/components/messages_test.py
@@ -6,48 +6,25 @@
 
 import pytest
 from shortfin_apps.llm.components.messages import InferenceExecRequest, InferencePhase
+from unittest.mock import patch
 
 
-@pytest.fixture
-def mock_request():
-    """
-    A mock InferenceExecRequest class that doesn't require worker context.
-
-    Useful for testing init and printing.
-    """
-    original_init = InferenceExecRequest.__init__
-
-    def mock_init(self, phase, input_token_ids, rid=None):
-        self.phase = phase
-        self.start_position = 0
-        self.input_token_ids = input_token_ids
-        self.rid = rid
-        self.return_all_logits = False
-        self.return_host_array = True
-        self.result_logits = None
-        self._cache = None
-        self.allocation = None
-
-    InferenceExecRequest.__init__ = mock_init
-    yield InferenceExecRequest
-    InferenceExecRequest.__init__ = original_init
-
-
-def test_inference_exec_request_repr(mock_request):
+@patch("shortfin.VoidFuture")
+def test_inference_exec_request_repr(mock_void_future):
     """
     Test the string representation of InferenceExecRequest in different states.
 
     This is useful for debugging and logging. Other test cases may depend on the debug log formats.
+
+    Patches shortfin.VoidFuture with a mock because we're not running this testcase on a worker thread.
     """
-    # default settings
-    req = mock_request(InferencePhase.PREFILL, [1, 2, 3, 4], rid="test123")
+    req = InferenceExecRequest(InferencePhase.PREFILL, [1, 2, 3, 4], rid="test123")
     assert (
         str(req)
         == "InferenceExecRequest[phase=P,pos=0,rid=test123,flags=host,input_token_ids=[1, 2, 3, 4]]"
     )
 
-    # mutated settings
-    req = mock_request(InferencePhase.DECODE, [], rid="test123")
+    req = InferenceExecRequest(InferencePhase.DECODE, [], rid="test123")
     req.return_host_array = False
     req.return_all_logits = False
     req.rid = None
