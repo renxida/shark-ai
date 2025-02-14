6ba3c1e419ef8c37f3f0018f3c153694fb6e4909
Cedar <cedar.ren@gmail.com>
2025-02-14 09:17:18 -0800

    fix another remote file interface problem

--- app_tests/integration_tests/llm/model_management.py
+++ app_tests/integration_tests/llm/model_management.py
@@ -430,7 +430,7 @@ TEST_MODELS[
 # TODO: upstream this to sharktank
 Dataset(
     "Mxode/TinyStories-LLaMA2-25M-256h-4l-GQA",
-    (
+    files = [
         RemoteFile(
             file_id = "model.safetensors",
             filename= "model.safetensors",
@@ -440,8 +440,8 @@ Dataset(
                 "tokenizer.json",
                 "tokenizer_config.json",
             ),
-        )
-    ),
+        ),
+    ]
 )
 
 TEST_MODELS["tinystories_llama2_25m"] = ModelConfig(
